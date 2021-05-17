86fa6373fb264df4a24b80af1897d845b11c3e50
Thomas Bianchi <thomas.bianchi8@gmail.com>
2021-05-17 09:15:23 +0200

    split config_schema

--- config.go
+++ config.go
@@ -9,33 +9,9 @@ import (
 
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
-	fhu "github.com/valyala/fasthttp/fasthttputil"
 	"gopkg.in/yaml.v2"
 )
 
-type config struct {
-	Listen      string
-	ListenPprof string `yaml:"listen_pprof"`
-
-	Target string
-
-	LogLevel        string `yaml:"log_level"`
-	Timeout         time.Duration
-	TimeoutShutdown time.Duration `yaml:"timeout_shutdown"`
-
-	Tenant struct {
-		Label       string `yaml:"label,omitempty"`
-		LabelRemove bool `yaml:"label_remove,omitempty"`
-		NamespaceLabel string `yaml:"namespace_label,omitempty"`
-		BatchSize int `yaml:"batch_size,omitempty"`
-		QueryInterval int `yaml:"query_interval,omitempty"`
-		Header      string
-		Default     string
-	}
-
-	pipeIn  *fhu.InmemoryListener
-	pipeOut *fhu.InmemoryListener
-}
 
 func configParse(b []byte) (*config, error) {
 	cfg := &config{}
@@ -55,12 +31,12 @@ func configParse(b []byte) (*config, error) {
 		cfg.Tenant.Label = "__tenant__"
 	}
 
-	AUTH_USER_PASS := os.Getenv("AUTH_USER_PASS")
-
 	u, err := url.Parse(cfg.Target)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	AUTH_USER_PASS := os.Getenv("AUTH_USER_PASS")
 
 	if AUTH_USER_PASS != "" {
 		u.User = url.UserPassword(strings.Split(AUTH_USER_PASS, ":")[0], strings.Split(AUTH_USER_PASS, ":")[1])
---
+++ config_schema.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"time"
+
+	fhu "github.com/valyala/fasthttp/fasthttputil"
+)
+
+type config struct {
+	Listen      string
+	ListenPprof string `yaml:"listen_pprof"`
+
+	Target string
+
+	LogLevel        string `yaml:"log_level"`
+	Timeout         time.Duration
+	TimeoutShutdown time.Duration `yaml:"timeout_shutdown"`
+
+	Tenant struct {
+		Label       string `yaml:"label,omitempty"`
+		LabelRemove bool `yaml:"label_remove,omitempty"`
+		NamespaceLabel string `yaml:"namespace_label,omitempty"`
+		BatchSize int `yaml:"batch_size,omitempty"`
+		QueryInterval int `yaml:"query_interval,omitempty"`
+		Header      string
+		Default     string
+	}
+
+	pipeIn  *fhu.InmemoryListener
+	pipeOut *fhu.InmemoryListener
+}
